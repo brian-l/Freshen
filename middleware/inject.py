d61f0d6f4c23a4aaf473a6c2390f1025b1343fb9
Brian Lee <brian@mutualmind.com>
2013-07-08 22:47:50 -0500

    Simplify injection process with try/except instead of a lot of if statements

--- middleware/inject.py
+++ middleware/inject.py
@@ -4,20 +4,17 @@ from lxml import html, etree
 class ScriptInject():
     def process_response(self, request, response):
         # is this a development environment?
-        if hasattr(settings, 'DEVELOPMENT'):
-            if settings.DEVELOPMENT == True:
-                # is the response valid?
-                if response.has_header('Content-type'):
-                    # don't try to inject a script into non-HTML
-                    if 'html' in response.get('Content-type'):
-                        tree = html.fromstring(response.content)
-                        head = tree.find('head')
-                        # if this is false the content is malformed, skip it
-                        if len(head):
-                            # create the freshen script element
-                            # freshen_script = etree.SubElement(head, 'script', type='text/javascript', charset='utf-8', src='%sfreshen.js' % (settings.STATIC_URL))
-                                    freshen_script = etree.SubElement(head, 'script', type='text/javascript', charset='utf-8', src='http://localhost:9020/freshen.js')
+        try:
+            if settings.DEVELOPMENT == True and 'html' in response['Content-type']:
+                tree = html.fromstring(response.content)
+                head = tree.find('head')
+                # if this is false the content is malformed, skip it
+                if len(head):
+                    # create the freshen script element
+                    freshen_script = etree.SubElement(head, 'script', type='text/javascript', charset='utf-8', src='http://localhost:9020/freshen.js')
 
-                            response.content = html.tostring(tree, pretty_print=True)
+                    response.content = html.tostring(tree, pretty_print=True)
+        except:
+            pass
 
         return response
